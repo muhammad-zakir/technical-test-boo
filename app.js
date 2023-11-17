729d2c8b16a4ed0d790977b23fe1d2e1d2734a1a
Muhammad Zakir <muhammad.zakir@seven-retail.com>
2023-11-17 22:58:58 +0700

    Initiate the MongoServer and add an inital seeder for the profiles

--- app.js
+++ app.js
@@ -3,13 +3,68 @@
 const express = require('express');
 const app = express();
 const port =  process.env.PORT || 3000;
+const { MongoClient } = require('mongodb');
+const { MongoMemoryServer } = require('mongodb-memory-server');
 
-// set the view engine to ejs
-app.set('view engine', 'ejs');
+MongoMemoryServer.create()
+    .then(async function(mongoServer)  {
+      // set the database's connection uri
+      app.set('databaseConnection', new MongoClient(mongoServer.getUri()));
 
-// routes
-app.use('/', require('./routes/profile')());
+      // seed the initial profiles
+      await app.get('databaseConnection').connect();
+      const database = app.get('databaseConnection').db('');
+      const profiles = database.collection('profiles');
+      await profiles.insertMany([
+        {
+          "identifier": 1,
+          "name": "A Martinez",
+          "description": "Adolph Larrue Martinez III.",
+          "mbti": "ISFJ",
+          "enneagram": "9w3",
+          "variant": "sp/so",
+          "tritype": 725,
+          "socionics": "SEE",
+          "sloan": "RCOEN",
+          "psyche": "FEVL",
+          "image": "https://soulverse.boo.world/images/1.png",
+        },
+        {
+          "identifier": 2,
+          "name": "Superion Blowpipe",
+          "description": "An Autobot from Earth, with an Fe- blood type.",
+          "mbti": "ISTJ",
+          "enneagram": "3w2",
+          "variant": "sp/so",
+          "tritype": 725,
+          "socionics": "SEE",
+          "sloan": "RCOEN",
+          "psyche": "FEVL",
+          "image": "https://soulverse.boo.world/images/1.png",
+        },
+        {
+          "identifier": 3,
+          "name": "Rumble Ramjet",
+          "description": "An Autobot from Cybertron, with an Au- blood type.",
+          "mbti": "INFP",
+          "enneagram": "2w3",
+          "variant": "sp/so",
+          "tritype": 725,
+          "socionics": "SEE",
+          "sloan": "RCOEN",
+          "psyche": "FEVL",
+          "image": "https://soulverse.boo.world/images/1.png",
+        }
+      ]);
 
-// start server
-const server = app.listen(port);
-console.log('Express started. Listening on %s', port);
+      // set the view engine to ejs
+      app.set('view engine', 'ejs');
+
+      // routes
+      app.use('/', require('./routes/profile')());
+
+      // start server
+      const server = app.listen(port);
+      console.log('Express started. Listening on %s', port);
+    })
+    .catch(error => { console.log(error) });
