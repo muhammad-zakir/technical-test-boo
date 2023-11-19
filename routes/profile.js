99701fb48f6381594c11fc7c207235b12eb1e751
Muhammad Zakir <muhammad.zakir@seven-retail.com>
2023-11-19 17:53:25 +0700

    Add redirect for handling default / root route

--- routes/profile.js
+++ routes/profile.js
@@ -6,6 +6,10 @@ const { createProfileHandler } = require('../handlers/createProfile');
 const { fetchProfileHandler } = require('../handlers/fetchProfile');
 
 module.exports = function() {
+  router.get('/', function (request, response) {
+    response.redirect('/1')
+  });
+
   router.get('/:userIdentifier(\\d+)', async function(request, response, next) {
     await fetchProfileHandler(request, response);
   });
