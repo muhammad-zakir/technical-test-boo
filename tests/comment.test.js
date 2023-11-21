bfbec7d7a1ea0fe3b4d7b1e06b9cb45fb91c7323
Muhammad Zakir <muhammad.zakir@seven-retail.com>
2023-11-21 17:50:59 +0700

    Increase code coverages

--- tests/comment.test.js
+++ tests/comment.test.js
@@ -72,6 +72,21 @@ describe('Comment Test Suite', () => {
     expect(response.statusCode).toBe(404);
   });
   
+  test('Test the POST method with a valid body but has an invalid enneagram', async () => {
+    const response = await request(application).post('/comment').set('Accept', 'application/json')
+      .send({
+        profileIdentifier: 10,
+        voterIdentifier: 2,
+        comment: "test",
+        votes: {
+          enneagram: "2w2",
+          mbti: "INFP",
+          zodiac: "Cancer"
+        }
+      });
+    expect(response.statusCode).toBe(422);
+  });
+
   test('Test the POST method with a valid body but has an invalid mbti', async () => {
     const response = await request(application).post('/comment').set('Accept', 'application/json')
       .send({
@@ -87,6 +102,86 @@ describe('Comment Test Suite', () => {
     expect(response.statusCode).toBe(422);
   });
   
+  test('Test the POST method with a valid body but without an enneagram', async () => {
+    const response = await request(application).post('/comment').set('Accept', 'application/json')
+      .send({
+        profileIdentifier: 1,
+        voterIdentifier: 2,
+        comment: "test",
+        votes: {
+          mbti: "INFP",
+          zodiac: "Cancer"
+        }
+      });
+    expect(response.statusCode).toBe(201);
+    expect(response.body.enneagram).toBe(null);
+  });
+
+  test('Test the POST method with a valid body but without a mbti', async () => {
+    const response = await request(application).post('/comment').set('Accept', 'application/json')
+      .send({
+        profileIdentifier: 1,
+        voterIdentifier: 2,
+        comment: "test",
+        votes: {
+          enneagram: "2w3",
+          zodiac: "Cancer"
+        }
+      });
+    expect(response.statusCode).toBe(201);
+    expect(response.body.mbti).toBe(null);
+  });
+
+  test('Test the POST method with a valid body but without a zodiac', async () => {
+    const response = await request(application).post('/comment').set('Accept', 'application/json')
+      .send({
+        profileIdentifier: 1,
+        voterIdentifier: 2,
+        comment: "test",
+        votes: {
+        }
+      });
+    expect(response.statusCode).toBe(201);
+  });
+
+  test('Test the POST method with a valid body but without a single vote', async () => {
+    const response = await request(application).post('/comment').set('Accept', 'application/json')
+      .send({
+        profileIdentifier: 1,
+        voterIdentifier: 2,
+        comment: "test",
+        votes: {}
+      });
+    expect(response.statusCode).toBe(201);
+    expect(response.body.zodiac).toBe(null);
+  });
+
+  test('Test the POST method with a valid body but with an invalid vote field', async () => {
+    const response = await request(application).post('/comment').set('Accept', 'application/json')
+      .send({
+        profileIdentifier: 1,
+        voterIdentifier: 2,
+        comment: "test",
+        votes: 'test'
+      });
+    expect(response.statusCode).toBe(422);
+  });
+
+  test('Test the POST method with a valid body but has an invalid zodiac', async () => {
+    const response = await request(application).post('/comment').set('Accept', 'application/json')
+      .send({
+        profileIdentifier: 10,
+        voterIdentifier: 2,
+        comment: "test",
+        votes: {
+          enneagram: "2w3",
+          mbti: "INFP",
+          zodiac: "Avicenna"
+        }
+      });
+    expect(response.statusCode).toBe(422);
+  });
+
   test('Test the POST method with an invalid body', async () => {
     const response = await request(application).post('/comment').set('Accept', 'application/json')
       .send({
@@ -109,7 +204,7 @@ describe('Comment Test Suite', () => {
         comment: "test",
         votes: {
           enneagram: "2w3",
-          mbti: "INFP",
+          mbti: "ISTJ",
           zodiac: "Cancer"
         }
       });
@@ -122,11 +217,58 @@ describe('Comment Test Suite', () => {
       });
     expect(response.statusCode).toBe(200);
   });
-  
-  test('Test the GET method with a valid profile identifier after another comment has been created', async () => {
-    const response = await request(application).get('/comment/1');
-    expect(response.statusCode).toBe(200);
-    expect(response.body.length).toStrictEqual(2);
+
+  test('Test the POST method for like with a valid body, but run twice so it should unlike it', async () => {
+    const createCommentResponse = await request(application).post('/comment').set('Accept', 'application/json')
+      .send({
+        profileIdentifier: 1,
+        voterIdentifier: 2,
+        comment: "test",
+        votes: {
+          enneagram: "2w3",
+          mbti: "INFP",
+          zodiac: "Cancer"
+        }
+      });
+    const commentIdentifier = createCommentResponse.body._id;
+    const profileIdentifier = 3;
+
+    const likeResponse = await request(application).post('/comment/like').set('Accept', 'application/json')
+      .send({
+        commentIdentifier: commentIdentifier,
+        profileIdentifier: profileIdentifier
+      });
+    expect(likeResponse.statusCode).toBe(200);
+
+    const unlikeResponse = await request(application).post('/comment/like').set('Accept', 'application/json')
+      .send({
+        commentIdentifier: commentIdentifier,
+        profileIdentifier: profileIdentifier
+      });
+    expect(unlikeResponse.statusCode).toBe(200);
+    expect(unlikeResponse.body.likes.length).toStrictEqual(0);
+  });
+
+  test('Test the POST method for like with a valid body but an invalid comment identifier', async () => {
+    const createCommentResponse = await request(application).post('/comment').set('Accept', 'application/json')
+      .send({
+        profileIdentifier: 1,
+        voterIdentifier: 2,
+        comment: "test",
+        votes: {
+          enneagram: "2w3",
+          mbti: "INFP",
+          zodiac: "Cancer"
+        }
+      });
+    const commentIdentifier = createCommentResponse.body._id;
+
+    const response = await request(application).post('/comment/like').set('Accept', 'application/json')
+      .send({
+        commentIdentifier: commentIdentifier,
+        profileIdentifier: 10
+      });
+    expect(response.statusCode).toBe(404);
   });
 
   test('Test the POST method for like with a valid body but an invalid comment identifier', async () => {
@@ -144,5 +286,48 @@ describe('Comment Test Suite', () => {
         commentIdentifier: 1
       });
     expect(response.statusCode).toBe(422);
+  });
+
+  test('Test the GET method with a filter by enneagram', async () => {
+    const response = await request(application).get('/comment/1').query({ filter: 'enneagram' });
+    expect(response.statusCode).toBe(200);
+    expect(response.body.length).toStrictEqual(5);
+  });
+
+  test('Test the GET method with a filtered by mbti', async () => {
+    const response = await request(application).get('/comment/1').query({ filter: 'mbti' });
+    expect(response.statusCode).toBe(200);
+    expect(response.body.length).toStrictEqual(5);
+  });
+
+  test('Test the GET method with a filtered by zodiac', async () => {
+    const response = await request(application).get('/comment/1').query({ filter: 'zodiac' });
+    expect(response.statusCode).toBe(200);
+    expect(response.body.length).toStrictEqual(6);
+  });
+
+  test('Test the GET method sorted by most likes', async () => {
+    const response = await request(application).get('/comment/1').query({ sort: 'best' });
+    expect(response.statusCode).toBe(200);
+    expect(response.body[0].likes).toStrictEqual(1);
+  });
+
+  test('Test the GET method sorted by most recent', async () => {
+    const createCommentResponse = await request(application).post('/comment').set('Accept', 'application/json')
+      .send({
+        profileIdentifier: 1,
+        voterIdentifier: 2,
+        comment: "test",
+        votes: {
+          enneagram: "2w3",
+          mbti: "INFP",
+          zodiac: "Cancer"
+        }
+      });
+    const commentTime = createCommentResponse.body.createdAt;
+
+    const response = await request(application).get('/comment/1').query({ sort: 'recent' });
+    expect(response.statusCode).toBe(200);
+    expect(response.body[0].createdAt).toStrictEqual(commentTime);
   });
 });
